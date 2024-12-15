86edf5408bb6679c5118bba453012a0953187f66
MihaellK <79888949+MihaellK@users.noreply.github.com>
2024-12-15 19:20:41 -0300

    init Board order System

---
+++ Unity Project/EscapeAsylumVR/Assets/Scripts/BoardPuzzle/BoardInteractor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardInteractor : MonoBehaviour
+{
+    [SerializeField] private Transform Note;
+
+    void Start()
+    {
+        
+    }
+}
